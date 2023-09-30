d6a7e92d5ceda4783f55426f7a4ba9b12321d445
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2023-09-30 03:58:13 +0100

    Add some classes

---
+++ src/main/java/me/chancesd/sdutils/updater/SpigotUpdater.java
@@ -0,0 +1,43 @@
+package me.chancesd.sdutils.updater;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.bukkit.plugin.Plugin;
+
+import me.chancesd.sdutils.utils.Log;
+
+public class SpigotUpdater extends Updater {
+
+	public SpigotUpdater(final Plugin plugin, final int id, final UpdateType type) {
+		super(plugin, id, type);
+	}
+
+	@Override
+	protected boolean read() {
+		try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.getId()).openStream();
+		        Scanner scanner = new Scanner(inputStream)) {
+			if (scanner.hasNext()) {
+				this.versionName = scanner.next();
+			}
+		} catch (final IOException e) {
+			Log.warning("Spigot might be down or have it's protection up! This error can be safely ignored");
+			this.setResult(UpdateResult.FAIL_DBO);
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public final boolean downloadFile() {
+		return false;
+	}
+
+	@Override
+	public String getUpdateLink() {
+		return "https://www.spigotmc.org/resources/" + getPlugin().getName() + "." + getId();
+	}
+
+}
---
+++ src/main/java/me/chancesd/sdutils/utils/Log.java
@@ -0,0 +1,56 @@
+package me.chancesd.sdutils.utils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+
+public final class Log {
+
+	private static Logger logger;
+	private static boolean debug;
+	private static String prefixMsg;
+
+	private Log() {
+	}
+
+	public static void setup(final Logger log, final String pluginPrefix) {
+		Log.logger = log;
+		Log.prefixMsg = pluginPrefix;
+	}
+
+	public static void infoColor(final String message) {
+		Bukkit.getConsoleSender().sendMessage(prefixMsg + " " + message);
+	}
+
+	public static void info(final String message) {
+		logger.info(message);
+	}
+
+	public static void severe(final String message) {
+		logger.severe(message);
+	}
+
+	public static void severe(final String message, final Throwable thrown) {
+		logger.log(Level.SEVERE, message, thrown);
+	}
+
+	public static void warning(final String message) {
+		logger.warning(message);
+	}
+
+	public static void warning(final String message, final Throwable thrown) {
+		logger.log(Level.WARNING, message, thrown);
+	}
+
+	public static void debug(final String message) {
+		if (debug) {
+			logger.info(message);
+		}
+	}
+
+	public static void setDebug(final boolean debug) {
+		Log.debug = debug;
+	}
+
+}
